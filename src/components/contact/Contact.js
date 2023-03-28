15952c994e255b69fb2fb8a087fea9e144b6c753
OLuciani <lucianioscar1@gmail.com>
2023-03-28 13:24:49 -0300

    Detalles Contact.css

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -12,36 +12,36 @@ const Contact = () => {
 
       <main className='div-box-contact container-fluid'>
          
-           <h2 className='text-center titulo-contact'>Información de contacto</h2>
+        <h2 className='text-center titulo-contact'>Información de contacto</h2>
 
-           <div className='box-p-icon'>
-            <i className="bi bi-geo-alt-fill"></i>
-            <p>Dirección: Sucre 327. Ciudad de Córdoba.<br/>Argentina</p>
-          </div> 
-          <br/>
+        <div className='box-p-icon'>
+          <i className="bi bi-geo-alt-fill"></i>
+          <p>Dirección: Sucre 327. Ciudad de Córdoba.<br/>Argentina</p>
+        </div> 
+        <br/>
 
-          <div className='box-p-icon'>
-            <i className="bi bi-telephone-fill" />
-            <p>Teléfono: 54 9 351 123456</p>
-          </div>   
-          <br/>
-        
-          <div className='box-p-icon'>   
-            <i className="bi bi-envelope-at-fill" />
-            <p>Email: lucianiautomoviles@gmail.com</p>
-          </div> 
-          <br/>
+        <div className='box-p-icon'>
+          <i className="bi bi-telephone-fill" />
+          <p>Teléfono: 54 9 351 123456</p>
+        </div>   
+        <br/>
+      
+        <div className='box-p-icon'>   
+          <i className="bi bi-envelope-at-fill" />
+          <p>Email: lucianiautomoviles@gmail.com</p>
+        </div> 
+        <br/>
 
-          <div className='box-p-icon'>
-            <i className="bi bi-facebook" /> 
-            <p>Facebook</p>
-          </div>
-          <br/>
+        <div className='box-p-icon'>
+          <i className="bi bi-facebook" /> 
+          <p>Facebook</p>
+        </div>
+        <br/>
 
-          <div className='box-p-icon'>
-            <i className="bi bi-instagram" />
-            <p>Instagram</p>
-          </div>
+        <div className='box-p-icon'>
+          <i className="bi bi-instagram" />
+          <p>Instagram</p>
+        </div>
 
       </main>
 
